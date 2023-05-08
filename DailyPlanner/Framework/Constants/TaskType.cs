98b864920dc3d723b9e7feddf360bd42759178dc
Lawrence Kimsey <lmkimsey@gmail.com>
2023-05-07 19:19:14 -0600

    Add task type enum

---
+++ DailyPlanner/Framework/Constants/TaskType.cs
@@ -0,0 +1,15 @@
+﻿namespace DailyPlanner.Framework.Constants
+{
+    /// <summary>A tab in the daily planner menu.</summary>
+    internal enum TaskType
+    {
+        /// <summary>A daily task.</summary>
+        Daily,
+
+        /// <summary>A weekly task.</summary>
+        Weekly,
+
+        /// <summary>A one-day task on a certain date.</summary>
+        OnDate
+    }
+}
