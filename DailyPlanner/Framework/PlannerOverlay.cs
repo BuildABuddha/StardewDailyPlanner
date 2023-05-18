eacfed98a1382111661dad85ab5e98d4bf37d21c
Lawrence Kimsey <34719668+BuildABuddha@users.noreply.github.com>
2023-05-17 22:23:21 -0600

    Keep overly string in memory

--- DailyPlanner/Framework/PlannerOverlay.cs
+++ DailyPlanner/Framework/PlannerOverlay.cs
@@ -13,6 +13,7 @@ using StardewModdingAPI;
 using StardewValley.Menus;
 using StardewValley.Objects;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections;
 
 namespace DailyPlanner.Framework
 {
@@ -29,12 +30,54 @@ namespace DailyPlanner.Framework
 
         private readonly ModConfig Config;
 
+        private string OverlayString;
+
+        // last known settings
+        private List<string> LastKnownPlanList = new();
+        private List<string> LastKnownCheckList = new();
+        private int LastKnownLineLength;
+        private int LastKnownLineCount;
+        private bool LastKnownShowPlannerSetting;
+        private bool LastKnownShowChecklistSetting;
+
         public PlannerOverlay(ModEntry modEntry, ModConfig config)
         {
             this.ModEntry = modEntry;
             this.Config = config;
         }
 
+        /// <summary>
+        /// Returns true if any of the things used to generate the overlay string have changed.
+        /// </summary>
+        /// <param name="todaysPlanList"></param>
+        /// <param name="checklist"></param>
+        /// <returns></returns>
+        private bool HasOverlayChanged(List<string> todaysPlanList, List<string> checklist)
+        {
+            if (!Enumerable.SequenceEqual(todaysPlanList, this.LastKnownPlanList)           // Has the plan list changed?
+                || !Enumerable.SequenceEqual(checklist, this.LastKnownCheckList)            // Has the checklist changed?
+                || !(this.Config.OverlayMaxLength == this.LastKnownLineLength)              // Has the settings for overlay max line length changed?
+                || !(this.Config.OverlayMaxLines == this.LastKnownLineCount)                // Has the settings for max lines on the overlay changed?
+                || !this.Config.ShowPlannerTasks == this.LastKnownShowPlannerSetting        // Has the settings for showing planner tasks changed?
+                || !this.Config.ShowChecklistTasks == this.LastKnownShowChecklistSetting){  // Has the settings for showing checklist tasks changed?
+
+                this.LastKnownPlanList = new(todaysPlanList);
+                this.LastKnownCheckList = new(checklist);
+                this.LastKnownLineLength = this.Config.OverlayMaxLength;
+                this.LastKnownLineCount = this.Config.OverlayMaxLines;
+                this.LastKnownShowPlannerSetting = this.Config.ShowPlannerTasks;
+                this.LastKnownShowChecklistSetting = this.Config.ShowChecklistTasks;
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Shortens a string so it can fit in a certain text box lenth. 
+        /// </summary>
+        /// <param name="text">Text to be shortened</param>
+        /// <param name="ListMaxHeaderLength">X measurement of a rectangle this text should fit into</param>
+        /// <returns>Shortened text with "..." at end</returns>
         private static string ConcatLine(string text, int ListMaxHeaderLength)
         {
             int length = text.Length;
@@ -45,57 +88,71 @@ namespace DailyPlanner.Framework
             return $"{text[..length]}...";
         }
 
+        /// <summary>
+        /// Returns the string that will get drawn on the overlay every frame.
+        /// </summary>
+        /// <param name="todaysPlanList">List of tasks from the daily planner</param>
+        /// <param name="checklist">List of tasks from the checklist</param>
+        /// <returns>Formatted string for the overlay with multiple lines</returns>
         private string GetFormattedList(List<string> todaysPlanList, List<string> checklist)
         {
-            StringBuilder stringBuilder = new();
-            int numLines = 0;
-            int ListMaxHeaderLength = (int)Game1.smallFont.MeasureString(new string('_', this.Config.OverlayMaxLength)).X;
-
-            // Add planner header if both options are enabled and planner list > 0
-            if(this.Config.ShowPlannerTasks && this.Config.ShowChecklistTasks 
-                && todaysPlanList.Count > 0)
+            // Rather than do all of this string building every single frame, we'll do a quick check to see
+            // if anything has actually changed since the last time we did this.
+            // This should be a LOT faster than building the string literally every frame.
+            if (this.HasOverlayChanged(todaysPlanList, checklist))
             {
-                stringBuilder.AppendLine("Planner:");
-                numLines++;
-            }
+                StringBuilder stringBuilder = new();
+                int numLines = 0;
+                int ListMaxHeaderLength = (int)Game1.smallFont.MeasureString(new string('_', this.Config.OverlayMaxLength)).X;
 
-            // Add tasks in daily planner if enabled, we have free lines, and planner list > 0
-            if (this.Config.ShowPlannerTasks 
-                && numLines < this.Config.OverlayMaxLines 
-                && todaysPlanList.Count > 0) {
-                foreach (string str in todaysPlanList)
+                // Add planner header if both options are enabled and planner list > 0
+                if(this.Config.ShowPlannerTasks && this.Config.ShowChecklistTasks 
+                    && todaysPlanList.Count > 0)
                 {
-                    if (Game1.smallFont.MeasureString($"• {str}").X < ListMaxHeaderLength) stringBuilder.AppendLine($"• {str}");
-                    else stringBuilder.AppendLine(ConcatLine($"• {str}", ListMaxHeaderLength));
+                    stringBuilder.AppendLine(this.ModEntry.Helper.Translation.Get("overlay.planner"));
                     numLines++;
-                    if (numLines == this.Config.OverlayMaxLines) break;
-                }                
-            }
-
-            // Add checklist header if both options are enabled, we have free lines, and checklist > 0
-            if (this.Config.ShowPlannerTasks && this.Config.ShowChecklistTasks 
-                && numLines < this.Config.OverlayMaxLines 
-                && checklist.Count > 0)
-            {
-                stringBuilder.AppendLine("Checklist:");
-                numLines++;
-            }
-
-            // Add tasks in checklist if enabled, we have free lines, and checklist > 0
-            if (this.Config.ShowChecklistTasks
-                && numLines < this.Config.OverlayMaxLines
-                && checklist.Count > 0)
-            {
-                foreach (string str in checklist)
-                {
-                    if (Game1.smallFont.MeasureString($"• {str}").X < ListMaxHeaderLength) stringBuilder.AppendLine($"• {str}");
-                    else stringBuilder.AppendLine(ConcatLine($"• {str}", ListMaxHeaderLength));
-                    numLines++;
-                    if (numLines == this.Config.OverlayMaxLines) break;
                 }
+
+                // Add tasks in daily planner if enabled, we have free lines, and planner list > 0
+                if (this.Config.ShowPlannerTasks 
+                    && numLines < this.Config.OverlayMaxLines 
+                    && todaysPlanList.Count > 0) {
+                    foreach (string str in todaysPlanList)
+                    {
+                        if (Game1.smallFont.MeasureString($"• {str}").X < ListMaxHeaderLength) stringBuilder.AppendLine($"• {str}");
+                        else stringBuilder.AppendLine(ConcatLine($"• {str}", ListMaxHeaderLength));
+                        numLines++;
+                        if (numLines == this.Config.OverlayMaxLines) break;
+                    }                
+                }
+
+                // Add checklist header if both options are enabled, we have free lines, and checklist > 0
+                if (this.Config.ShowPlannerTasks && this.Config.ShowChecklistTasks 
+                    && numLines < this.Config.OverlayMaxLines 
+                    && checklist.Count > 0)
+                {
+                    stringBuilder.AppendLine(this.ModEntry.Helper.Translation.Get("overlay.checklist"));
+                    numLines++;
+                }
+
+                // Add tasks in checklist if enabled, we have free lines, and checklist > 0
+                if (this.Config.ShowChecklistTasks
+                    && numLines < this.Config.OverlayMaxLines
+                    && checklist.Count > 0)
+                {
+                    foreach (string str in checklist)
+                    {
+                        if (Game1.smallFont.MeasureString($"• {str}").X < ListMaxHeaderLength) stringBuilder.AppendLine($"• {str}");
+                        else stringBuilder.AppendLine(ConcatLine($"• {str}", ListMaxHeaderLength));
+                        numLines++;
+                        if (numLines == this.Config.OverlayMaxLines) break;
+                    }
+                }
+
+                this.OverlayString = stringBuilder.ToString();
             }
 
-            return  stringBuilder.ToString();
+            return this.OverlayString;
         }
 
         public void Draw(SpriteBatch spriteBatch)
